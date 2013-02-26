64bf914aea0c692784df53f844173bc9356d2899
Sébastien SAUVAGE <sebsauvage@sebsauvage.net>
2013-02-26 16:03:47 +0100

    Corrected bug in cache purge.

--- index.php
+++ index.php
@@ -172,7 +172,7 @@ class pageCache
         if (is_dir($GLOBALS['config']['PAGECACHE']))
         {
             $handler = opendir($GLOBALS['config']['PAGECACHE']);
-            if ($handle!==false)
+            if ($handler!==false)
             {
                 while (($filename = readdir($handler))!==false) 
                 {
