27f27f6fe75adb9fa29767a4b4344e8243496468
Nono-m0le <nono@m0le.net>
2013-04-08 19:11:47 +0300

    Update index.php
    Dans l'idée, quand la description est vide, il affiche le titre :)

--- index.php
+++ index.php
@@ -2439,7 +2439,7 @@ function genList()
                 $date=$link['linkdate'];
                 if ($from <= $date && $date <= $to)
                 {
-                        array_push($linkarray, ''.$link['description'].' [<a href="'.$link['url'].'" target="_blank">'.$date.'</a>]');
+                        array_push($linkarray, ''.$link['description']==''?$link['title']:$link['description'].' [<a href="'.$link['url'].'" target="_blank">'.$date.'</a>]');
                         foreach (explode(' ',$link['tags']) as $t)
                                 array_push($tagcloud, $t);
                 }
