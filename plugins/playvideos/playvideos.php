840caea64f8c37f035fb3525f63153eb68492731
ArthurHoaro <arthur@hoa.ro>
2015-11-07 16:30:56 +0100

    Add unit tests for Playvideos plugin + coding style

--- plugins/playvideos/playvideos.php
+++ plugins/playvideos/playvideos.php
@@ -1,12 +1,20 @@
 <?php
+/**
+ * Plugin PlayVideos
+ *
+ * Add a button in the toolbar allowing to watch all videos.
+ * Note: this plugin adds jQuery.
+ */
 
 /**
  * When linklist is displayed, add play videos to header's toolbar.
  *
  * @param array $data - header data.
+ *
  * @return mixed - header data with playvideos toolbar item.
  */
-function hook_playvideos_render_header($data) {
+function hook_playvideos_render_header($data)
+{
     if ($data['_PAGE_'] == Router::$PAGE_LINKLIST) {
         $data['buttons_toolbar'][] = file_get_contents(PluginManager::$PLUGINS_PATH . '/playvideos/playvideos.html');
     }
@@ -18,9 +26,11 @@ function hook_playvideos_render_header($data) {
  * When linklist is displayed, include playvideos JS files.
  *
  * @param array $data - footer data.
+ *
  * @return mixed - footer data with playvideos JS files added.
  */
-function hook_playvideos_render_footer($data) {
+function hook_playvideos_render_footer($data)
+{
     if ($data['_PAGE_'] == Router::$PAGE_LINKLIST) {
         $data['js_files'][] = PluginManager::$PLUGINS_PATH . '/playvideos/jquery-1.11.2.min.js';
         $data['js_files'][] = PluginManager::$PLUGINS_PATH . '/playvideos/youtube_playlist.js';
