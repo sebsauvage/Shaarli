9a364c283e3e9b5ccb5fc7a9a8e2946b1918c6a5
ArthurHoaro <arthur@hoa.ro>
2015-11-07 16:37:23 +0100

    Add unit test for archiveorg plugin + coding style

--- plugins/archiveorg/archiveorg.php
+++ plugins/archiveorg/archiveorg.php
@@ -1,12 +1,19 @@
 <?php
+/**
+ * Plugin Archive.org.
+ *
+ * Add an icon in the link list for archive.org.
+ */
 
 /**
  * Add archive.org icon to link_plugin when rendering linklist.
  *
- * @param $data - linklist data.
+ * @param mixed $data - linklist data.
+ *
  * @return mixed - linklist data with archive.org plugin.
  */
-function hook_archiveorg_render_linklist($data) {
+function hook_archiveorg_render_linklist($data)
+{
     $archive_html = file_get_contents(PluginManager::$PLUGINS_PATH . '/archiveorg/archiveorg.html');
 
     foreach ($data['links'] as &$value) {
