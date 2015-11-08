1696f6aa07734c7502702cf810c9fdc93532ef56
ArthurHoaro <arthur@hoa.ro>
2015-11-08 12:00:06 +0100

    unit tests for the wallabag plugin + removed exit error if the config is not found + coding style

--- plugins/wallabag/wallabag.php
+++ plugins/wallabag/wallabag.php
@@ -1,25 +1,33 @@
 <?php
 
+/**
+ * Plugin Wallabag.
+ */
+
 // don't raise unnecessary warnings
 if (is_file(PluginManager::$PLUGINS_PATH . '/wallabag/config.php')) {
     include PluginManager::$PLUGINS_PATH . '/wallabag/config.php';
 }
 
 if (!isset($GLOBALS['plugins']['WALLABAG_URL'])) {
-    header('Content-Type: text/plain; charset=utf-8');
-    echo 'Wallabag plugin error: '. PHP_EOL;
-    echo '  Please copy "plugins/wallabag/config.php.dist" to config.php and configure your Wallabag URL.'. PHP_EOL;
-    echo '  You can also define "$GLOBALS[\'plugins\'][\'WALLABAG_URL\']" in your global Shaarli config.php file.';
-    exit;
+    $GLOBALS['plugins']['errors'][] = 'Wallabag plugin error: '.
+        'Please define "$GLOBALS[\'plugins\'][\'WALLABAG_URL\']" '.
+        'in "plugins/wallabag/config.php" or in your Shaarli config.php file.';
 }
 
 /**
  * Add wallabag icon to link_plugin when rendering linklist.
  *
- * @param $data - linklist data.
+ * @param mixed $data - linklist data.
+ *
  * @return mixed - linklist data with wallabag plugin.
  */
-function hook_wallabag_render_linklist($data) {
+function hook_wallabag_render_linklist($data)
+{
+    if (!isset($GLOBALS['plugins']['WALLABAG_URL'])) {
+        return $data;
+    }
+
     $wallabag_html = file_get_contents(PluginManager::$PLUGINS_PATH . '/wallabag/wallabag.html');
 
     foreach ($data['links'] as &$value) {
