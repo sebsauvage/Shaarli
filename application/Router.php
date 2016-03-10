8395d0b76145969f4b8940a415af5e46528f04a5
ArthurHoaro <arthur@hoa.ro>
2016-03-10 18:48:21 +0100

    Adds a route for ATOM and RSS feeds page

--- application/Router.php
+++ application/Router.php
@@ -15,6 +15,10 @@ class Router
 
     public static $PAGE_DAILY = 'daily';
 
+    public static $PAGE_FEED_ATOM = 'atom';
+
+    public static $PAGE_FEED_RSS = 'rss';
+
     public static $PAGE_TOOLS = 'tools';
 
     public static $PAGE_CHANGEPASSWORD = 'changepasswd';
@@ -77,6 +81,14 @@ class Router
 
         if (startsWith($query, 'do='. self::$PAGE_DAILY)) {
             return self::$PAGE_DAILY;
+        }
+
+        if (startsWith($query, 'do='. self::$PAGE_FEED_ATOM)) {
+            return self::$PAGE_FEED_ATOM;
+        }
+
+        if (startsWith($query, 'do='. self::$PAGE_FEED_RSS)) {
+            return self::$PAGE_FEED_RSS;
         }
 
         // At this point, only loggedin pages.
