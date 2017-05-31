807cade64c571929dc19afe3d44787c5abe84f57
ArthurHoaro <arthur@hoa.ro>
2017-05-31 17:50:11 +0200

    Add creation date when editing a link
    Also, alter the title on edition
    
    Fixes #431

--- index.php
+++ index.php
@@ -1431,7 +1431,7 @@ function renderPage($conf, $pluginManager, $LINKSDB, $history)
                 'url' => $url,
                 'description' => $description,
                 'tags' => $tags,
-                'private' => $private
+                'private' => $private,
             );
         } else {
             $link['linkdate'] = $link['created']->format(LinkDB::LINK_DATE_FORMAT);
