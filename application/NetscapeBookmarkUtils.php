66e74d50d38a6fea8fc904a1746157633de7cc65
ArthurHoaro <arthur@hoa.ro>
2017-10-07 16:40:16 +0200

    Don't write History for link import
    With large imports it has a large impact on performances and isn't really useful.
    
    Instead, write an IMPORT event, which let client using the history service resync its DB.
    
    -> 15k link import done in 6 seconds.
    
    Fixes #985

--- application/NetscapeBookmarkUtils.php
+++ application/NetscapeBookmarkUtils.php
@@ -66,6 +66,7 @@ class NetscapeBookmarkUtils
      * @param int    $importCount    how many links were imported
      * @param int    $overwriteCount how many links were overwritten
      * @param int    $skipCount      how many links were skipped
+     * @param int    $duration       how many seconds did the import take
      *
      * @return string Summary of the bookmark import status
      */
@@ -74,14 +75,16 @@ class NetscapeBookmarkUtils
         $filesize,
         $importCount=0,
         $overwriteCount=0,
-        $skipCount=0
+        $skipCount=0,
+        $duration=0
     )
     {
         $status = 'File '.$filename.' ('.$filesize.' bytes) ';
         if ($importCount == 0 && $overwriteCount == 0 && $skipCount == 0) {
             $status .= 'has an unknown file format. Nothing was imported.';
         } else {
-            $status .= 'was successfully processed: '.$importCount.' links imported, ';
+            $status .= 'was successfully processed in '. $duration .' seconds: ';
+            $status .= $importCount.' links imported, ';
             $status .= $overwriteCount.' links overwritten, ';
             $status .= $skipCount.' links skipped.';
         }
@@ -101,6 +104,7 @@ class NetscapeBookmarkUtils
      */
     public static function import($post, $files, $linkDb, $conf, $history)
     {
+        $start = time();
         $filename = $files['filetoupload']['name'];
         $filesize = $files['filetoupload']['size'];
         $data = file_get_contents($files['filetoupload']['tmp_name']);
@@ -184,7 +188,6 @@ class NetscapeBookmarkUtils
                 $linkDb[$existingLink['id']] = $newLink;
                 $importCount++;
                 $overwriteCount++;
-                $history->updateLink($newLink);
                 continue;
             }
 
@@ -196,16 +199,19 @@ class NetscapeBookmarkUtils
             $newLink['shorturl'] = link_small_hash($newLink['created'], $newLink['id']);
             $linkDb[$newLink['id']] = $newLink;
             $importCount++;
-            $history->addLink($newLink);
         }
 
         $linkDb->save($conf->get('resource.page_cache'));
+        $history->importLinks();
+
+        $duration = time() - $start;
         return self::importStatus(
             $filename,
             $filesize,
             $importCount,
             $overwriteCount,
-            $skipCount
+            $skipCount,
+            $duration
         );
     }
 }
