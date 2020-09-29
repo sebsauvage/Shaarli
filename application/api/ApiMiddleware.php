676571dab927b0fb9b3746c36f0d7540e8dba2b5
Christoph Stoettner <christoph.stoettner@stoeps.de>
2020-09-29 12:15:04 +0200

    Workaround for hoster (ionos)
    The hoster writes the environment variable with bearer token to
    REDIRECT_HTTP_AUTHORIZATION and needs to provide RewriteBase / to
    .htaccess

--- application/api/ApiMiddleware.php
+++ application/api/ApiMiddleware.php
@@ -107,7 +107,7 @@ class ApiMiddleware
      */
     protected function checkToken($request)
     {
-        if (! $request->hasHeader('Authorization')) {
+        if (! $request->hasHeader('Authorization') && !isset($_SERVER['REDIRECT_HTTP_AUTHORIZATION'])) {
             throw new ApiAuthorizationException('JWT token not provided');
         }
 
@@ -115,7 +115,11 @@ class ApiMiddleware
             throw new ApiAuthorizationException('Token secret must be set in Shaarli\'s administration');
         }
 
-        $authorization = $request->getHeaderLine('Authorization');
+	if (isset($_SERVER['REDIRECT_HTTP_AUTHORIZATION'])) {
+	    $authorization = $_SERVER['REDIRECT_HTTP_AUTHORIZATION'];
+	} else {
+            $authorization = $request->getHeaderLine('Authorization');
+	}
 
         if (! preg_match('/^Bearer (.*)/i', $authorization, $matches)) {
             throw new ApiAuthorizationException('Invalid JWT header');
