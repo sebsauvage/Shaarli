a1cd7a3b2ff32bf6a0f6083007f59104a85eb4bf
ArthurHoaro <arthur@hoa.ro>
2021-02-04 10:53:23 +0100

    ShaarliParsedown: add PHPDoc/comments

--- application/formatter/Parsedown/ShaarliParsedown.php
+++ application/formatter/Parsedown/ShaarliParsedown.php
@@ -4,6 +4,11 @@ declare(strict_types=1);
 
 namespace Shaarli\Formatter\Parsedown;
 
+/**
+ * Parsedown extension for Shaarli.
+ *
+ * Extension for both Parsedown and ParsedownExtra centralized in ShaarliParsedownTrait.
+ */
 class ShaarliParsedown extends \Parsedown
 {
     use ShaarliParsedownTrait;
--- application/formatter/Parsedown/ShaarliParsedownExtra.php
+++ application/formatter/Parsedown/ShaarliParsedownExtra.php
@@ -4,6 +4,11 @@ declare(strict_types=1);
 
 namespace Shaarli\Formatter\Parsedown;
 
+/**
+ * ParsedownExtra extension for Shaarli.
+ *
+ * Extension for both Parsedown and ParsedownExtra centralized in ShaarliParsedownTrait.
+ */
 class ShaarliParsedownExtra extends \ParsedownExtra
 {
     use ShaarliParsedownTrait;
